c4a0af977a5c2df2d1b7d29566156404e5023765
engel03455 <engel03455@chuh.org>
2021-11-22 09:04:23 -0500

    Update World Language Games Automation for -Basketball-.user.js

--- World Language Games Automation for -Basketball-.user.js
+++ World Language Games Automation for -Basketball-.user.js
@@ -1,13 +1,13 @@
 // ==UserScript==
 // @name         World Language Games Automation for "Basketball"
-// @namespace    none
+// @namespace    http://www.github.com/engel03455
 // @version      1.0
 // @description  This script will automatically complete the game "Basketball" as soon as it is launched, giving the maximum of 4 points.
-// @author       Andrew Engelmann
+// @author       http://www.github.com/engel03455
 // @match        https://wlangames.net/Basketball2.php*
 // @icon         https://www.google.com/s2/favicons?domain=wlangames.net
 // @grant        none
 // ==/UserScript==
 
 TotalPoints = 36;
-GotoCongratulations()
+GotoCongratulations();
