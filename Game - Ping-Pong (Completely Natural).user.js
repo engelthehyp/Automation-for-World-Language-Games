a32fbcfcdfa91cd9aa9ba12cc5d02ec0da6ab63f
engel03455 <engel03455@chuh.org>
2021-11-22 12:11:19 -0500

    Update Game - Ping-Pong (Completely Natural).user.js
    Wrapped code in anonymous function and set run-at for better compatibility.

--- Game - Ping-Pong (Completely Natural).user.js
+++ Game - Ping-Pong (Completely Natural).user.js
@@ -7,50 +7,53 @@
 // @match        *://wlangames.net/PingPong.php*
 // @icon         https://www.google.com/s2/favicons?domain=wlangames.net
 // @grant        none
+// @run-at       document-start
 // ==/UserScript==
 
-function getRandomInt(min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-}
+(function() {
+	function getRandomInt(min, max) {
+    	min = Math.ceil(min);
+    	max = Math.floor(max);
+    	return Math.floor(Math.random() * (max - min + 1)) + min;
+	}
 
-function CheckAnswer () {
-    //if(CurrentAnswer == 0)
-    //return(WrongAnswer);
-    var obj = document.getElementById("Paddle"+CurrentAnswer);
+	function CheckAnswer () {
+    	//if(CurrentAnswer == 0)
+    	//return(WrongAnswer);
+    	var obj = document.getElementById("Paddle"+CurrentAnswer);
 
-    //if(obj != null)
-    //if(obj.innerHTML == AnswerIs)
-    return(WrongAnswer-1); // Correct Answer!
+    	//if(obj != null)
+    	//if(obj.innerHTML == AnswerIs)
+    	return(WrongAnswer-1); // Correct Answer!
 
-    //return(WrongAnswer);
-    ShouldEndGame();
-}
+    	//return(WrongAnswer);
+    	ShouldEndGame();
+	}
 
-var naturalending = getRandomInt(32,34);
-//alert(naturalending+" guesses will be made before the game is ended");
+	var naturalending = getRandomInt(32,34);
+	//alert(naturalending+" guesses will be made before the game is ended");
 
-function ShouldEndGame () {
-    var guesses = document.getElementById("Guesses");
-    if (guesses >= naturalending) {
-       return(WrongAnswer);
-   }
-}
+	function ShouldEndGame () {
+    	var guesses = document.getElementById("Guesses");
+    	if (guesses >= naturalending) {
+       	return(WrongAnswer);
+   		}
+	}
 
-addJS_Node (CheckAnswer);
+	addJS_Node (CheckAnswer);
 
-function addJS_Node (text, s_URL, funcToRun, runOnLoad) {
-    var D                                   = document;
-    var scriptNode                          = D.createElement ('script');
-    if (runOnLoad) {
-        scriptNode.addEventListener ("load", runOnLoad, false);
-    }
-    scriptNode.type                         = "text/javascript";
-    if (text)       scriptNode.textContent  = text;
-    if (s_URL)      scriptNode.src          = s_URL;
-    if (funcToRun)  scriptNode.textContent  = '(' + funcToRun.toString() + ')()';
+	function addJS_Node (text, s_URL, funcToRun, runOnLoad) {
+    	var D                                   = document;
+    	var scriptNode                          = D.createElement ('script');
+    	if (runOnLoad) {
+        	scriptNode.addEventListener ("load", runOnLoad, false);
+    	}
+    	scriptNode.type                         = "text/javascript";
+    	if (text)       scriptNode.textContent  = text;
+    	if (s_URL)      scriptNode.src          = s_URL;
+    	if (funcToRun)  scriptNode.textContent  = '(' + funcToRun.toString() + ')()';
 
-    var targ = D.getElementsByTagName ('head')[0] || D.body || D.documentElement;
-    targ.appendChild (scriptNode);
-}
+    	var targ = D.getElementsByTagName ('head')[0] || D.body || D.documentElement;
+    	targ.appendChild (scriptNode);
+	}
+})();
