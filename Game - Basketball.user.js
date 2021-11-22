8c3f3676bef56d3056b86c9415140afb3f44be09
engel03455 <engel03455@chuh.org>
2021-11-22 12:00:46 -0500

    Update Game - Basketball.user.js
    Added run-at for better compatibility.

--- Game - Basketball.user.js
+++ Game - Basketball.user.js
@@ -7,6 +7,7 @@
 // @match        *://wlangames.net/Basketball2.php*
 // @icon         https://www.google.com/s2/favicons?domain=wlangames.net
 // @grant        none
+// @run-at       document-end
 // ==/UserScript==
 
 (function() {
