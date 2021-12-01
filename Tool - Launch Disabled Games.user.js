55aac90a11577f1bae110e32671a5d0fa66a500b
engel03455 <engel03455@chuh.org>
2021-12-01 12:03:07 -0500

    Update Tool - Launch Disabled Games.user.js
    Changed functionality of program from launching at context menu to launching when a keyboard shortcut is pressed.

--- Tool - Launch Disabled Games.user.js
+++ Tool - Launch Disabled Games.user.js
@@ -5,13 +5,13 @@
 // @downloadURL  https://github.com/engel03455/Automation-for-World-Language-Games/raw/main/Tool%20-%20Launch%20Disabled%20Games.user.js
 // @updateURL    https://github.com/engel03455/Automation-for-World-Language-Games/raw/main/Tool%20-%20Launch%20Disabled%20Games.user.js
 // @version      1.2
-// @description  Allows you to launch disabled games from the context menu on the World Language Games homepage.
+// @description  Allows you to launch disabled games by pressing the ALT + L keyboard shortcut on the World Language Games homepage.
 // @author       https://github.com/engel03455/
 // @match        *://wlangames.net/*
 // @exclude      *://wlangames.net/PlayGame.php*
 // @icon         https://www.google.com/s2/favicons?domain=wlangames.net
 // @grant        none
-// @run-at       context-menu
+// @run-at       document-start
 // ==/UserScript==
 
 //Find 'Em - SingleConcentration();
@@ -19,21 +19,33 @@
 //Car Puzzle - gotoPlayCarPuzzle();
 
 (function() {
-	var GameToLaunch = prompt("Which disabled game should be launched?\n\n0 - Cancel\n1 - Find'em\n2 - Double Find'em\n3 - Car Puzzle\n\nAnswering blank or anything else will also cancel.");
-	if (GameToLaunch == 0) {
-    	//DO NOTHING
+	function launcher(){
+		var GameToLaunch = prompt("Which disabled game should be launched?\n\n0 - Cancel\n1 - Find'em\n2 - Double Find'em\n3 - Car Puzzle\n\nAnswering blank or anything else will also cancel.");
+		
+		if (GameToLaunch == 0) {
+    		//DO NOTHING
+		}
+		else if (GameToLaunch == 1) {
+    		SingleConcentration(); //Launches "Find'em"
+		}
+		else if (GameToLaunch == 2) {
+    		DoubleConcentration(); //Launches "Double Find'em"
+		}
+		else if (GameToLaunch == 3) {
+    		gotoPlayCarPuzzle(); //Launches "Car Puzzle"
+		}
+		else {
+    		//DO NOTHING
+    		//alert("Please send a valid response. Exiting...");
+		}
 	}
-	else if (GameToLaunch == 1) {
-    	SingleConcentration(); //Launches "Find'em"
-	}
-	else if (GameToLaunch == 2) {
-    	DoubleConcentration(); //Launches "Double Find'em"
-	}
-	else if (GameToLaunch == 3) {
-    	gotoPlayCarPuzzle(); //Launches "Car Puzzle"
-	}
-	else {
-    	//DO NOTHING
-    	//alert("Please send a valid response. Exiting...");
-	}
+    document.onkeyup=function(e){
+	e = e || window.event; // for IE to cover IEs window object
+    if(e.altKey && e.which == 76) {
+        //alert('Keyboard shortcut working!');
+        launcher();
+        return false;
+    }
+}
+}
 })();
