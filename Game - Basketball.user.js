354df51f33a2ad8aa50cd9d38599b6632b42662e
engel03455 <engel03455@chuh.org>
2022-02-07 09:42:05 -0500

    Currently not functional
    Due to the game "basketball" being currently under construction at the WLG site, this script will not work for you because the game cannot be accessed. I do not know if it will still work after the game leaves construction, but I will check and fix it as required.

--- Game - Basketball.user.js
+++ Game - Basketball.user.js
@@ -1,11 +1,11 @@
 // ==UserScript==
-// @name         World Language Games Automation for "Basketball"
+// @name         World Language Games Automation for "Basketball" (NOT CURRENTLY FUNCITONAL, READ COMMIT MESSAGE)
 // @license      MIT
 // @namespace    http://www.github.com/engel03455
 // @downloadURL  https://github.com/engel03455/Automation-for-World-Language-Games/raw/main/Game%20-%20Basketball.user.js
 // @updateURL    https://github.com/engel03455/Automation-for-World-Language-Games/raw/main/Game%20-%20Basketball.user.js
 // @version      1.0
-// @description  This script will automatically complete the game "Basketball" as soon as it is launched, giving the maximum of 4 points.
+// @description  This script will automatically complete the game "Basketball" as soon as it is launched, giving the maximum of 4 points. (NOT CURRENTLY FUNCITONAL, READ COMMIT MESSAGE)
 // @author       http://www.github.com/engel03455
 // @match        *://wlangames.net/Basketball2.php*
 // @icon         https://www.google.com/s2/favicons?domain=wlangames.net
