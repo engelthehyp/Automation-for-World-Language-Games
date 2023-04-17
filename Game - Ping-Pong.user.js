1582e232aa92383855105c810db49eb1fd5ab043
engelthehyp <engel03455@chuh.org>
2023-04-17 11:22:29 -0400

    Added "use strict" and more descriptive code.

--- Game - Ping-Pong.user.js
+++ Game - Ping-Pong.user.js
@@ -4,7 +4,7 @@
 // @namespace    https://github.com/engel03455/Automation-for-World-Language-Games
 // @downloadURL  https://github.com/engel03455/Automation-for-World-Language-Games/raw/main/Game%20-%20Ping-Pong.user.js
 // @updateURL    https://github.com/engel03455/Automation-for-World-Language-Games/raw/main/Game%20-%20Ping-Pong.user.js
-// @version      2.0
+// @version      2.1
 // @description  Sends the signal for correct answer no matter what answer is picked (Correct Answer, Wrong Answer, No Answer) and automatically ends the game once 32 to 35 vollies have been made.
 // @author       http://www.github.com/engel03455
 // @match        *://wlangames.net/PingPong.php*
@@ -13,18 +13,21 @@
 // @run-at       document-start
 // ==/UserScript==
 
-(function() {
+(function () {
+	"use strict";
+
+	const rightAnswer = window.WrongAnswer - 1;
+	const wrongAnswer = window.WrongAnswer;
+	const turnsToTake = getRandomInt(32, 35);
+	
+	window.CheckAnswer = function () {
+		const turnsTaken = document.getElementById("Guesses").innerHTML;
+		return parseInt(turnsTaken) < turnsToTake ? rightAnswer: wrongAnswer;
+	}
+
 	function getRandomInt(min, max) {
 		min = Math.ceil(min);
 		max = Math.floor(max);
 		return Math.floor(Math.random() * (max - min + 1)) + min;
-	}
-
-	var naturalEnding = getRandomInt(32, 35);
-	
-	window.CheckAnswer = function() {
-		var guesses = document.getElementById("Guesses").innerHTML;
-		return (parseInt(guesses) >= naturalEnding) ?
-			(window.WrongAnswer) : (window.WrongAnswer - 1);
 	}
 })();
