7a26d00405ff85c8dfaa252ba22faaa50d4b1791
engelthehyp <engelthehyp@gmail.com>
2023-11-29 15:28:48 -0500

    Fixed info column in Check Points reading "undefined undefined". The script is now in a release state.

--- Game - Basketball.user.js
+++ Game - Basketball.user.js
@@ -5,7 +5,7 @@
 // @downloadURL  https://github.com/engelthehyp/Automation-for-World-Language-Games/raw/main/Game%20-%20Basketball.user.js
 // @updateURL    https://github.com/engelthehyp/Automation-for-World-Language-Games/raw/main/Game%20-%20Basketball.user.js
 // @description  This script will automatically complete the game "Basketball" as soon as it is launched, giving the maximum of 3 points by default. You can change the number of points given to be any number, but under normal circumstances the maximum number of points is 3, so if you don't want to blow your cover, it may be better keep it that way. PLEASE NOTE - This script is defunct and was made for an older version of Basketball that has since been updated.
-// @version      0.1
+// @version      1.0
 // @author       http://www.github.com/engelthehyp
 // @match        *://wlangames.net/NewBasketball.php*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=wlangames.net
@@ -13,13 +13,16 @@
 // @run-at       document-end
 // ==/UserScript==
 
-// TODO: "Info" column in check points using this method reads "	   undefined undefined   	". I will have to find a solution to that.
 (function() {
     'use strict';
 
+    const earnedPoints = 3;
+    const getVocabularyIndex = () => getRandomInt(0, window.Vocabulary.length - 1);
+
     [window.minutes, window.seconds] = [getRandomInt(1, 3), getRandomInt(0, 59)];
     window.OneSecond();
-    window.SpaceAdventuresSentenceGetServerKeys(3);
+    window.Words = [getVocabularyIndex(), getVocabularyIndex()];
+    window.SpaceAdventuresSentenceGetServerKeys(earnedPoints);
 
     function getRandomInt(min, max) {
         min = Math.ceil(min);
