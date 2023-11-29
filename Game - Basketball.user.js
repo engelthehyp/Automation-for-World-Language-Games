19872607ab49be1179684f291f8413265a67a972
engelthehyp <engelthehyp@gmail.com>
2023-11-29 15:30:23 -0500

    Mistakenly left the "defunct" message in the description. It's gone now.

--- Game - Basketball.user.js
+++ Game - Basketball.user.js
@@ -4,7 +4,7 @@
 // @namespace    https://github.com/engelthehyp/Automation-for-World-Language-Games
 // @downloadURL  https://github.com/engelthehyp/Automation-for-World-Language-Games/raw/main/Game%20-%20Basketball.user.js
 // @updateURL    https://github.com/engelthehyp/Automation-for-World-Language-Games/raw/main/Game%20-%20Basketball.user.js
-// @description  This script will automatically complete the game "Basketball" as soon as it is launched, giving the maximum of 3 points by default. You can change the number of points given to be any number, but under normal circumstances the maximum number of points is 3, so if you don't want to blow your cover, it may be better keep it that way. PLEASE NOTE - This script is defunct and was made for an older version of Basketball that has since been updated.
+// @description  This script will automatically complete the game "Basketball" as soon as it is launched, giving the maximum of 3 points by default. You can change the number of points given to be any number, but under normal circumstances the maximum number of points is 3, so if you don't want to blow your cover, it may be better keep it that way.
 // @version      1.0
 // @author       http://www.github.com/engelthehyp
 // @match        *://wlangames.net/NewBasketball.php*
