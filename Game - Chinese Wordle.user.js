f9827eeca042e53d5c2618515cd3b6ee626fbc22
engelthehyp <engelthehyp@gmail.com>
2023-11-30 14:33:00 -0500

    Fixed URL error in Chinese Wordle

--- Game - Chinese Wordle.user.js
+++ Game - Chinese Wordle.user.js
@@ -2,8 +2,8 @@
 // @name         World Language Games - Automation for "Chinese Wordle"
 // @license      MIT
 // @namespace    https://github.com/engelthehyp/Automation-for-World-Language-Games
-// @downloadURL  https://github.com/engelthehyp/Automation-for-World-Language-Games/raw/main/Game%20-%20Basketball.user.js
-// @updateURL    https://github.com/engelthehyp/Automation-for-World-Language-Games/raw/main/Game%20-%20Basketball.user.js
+// @downloadURL  https://github.com/engelthehyp/Automation-for-World-Language-Games/raw/main/Game%20-%20Chinese%20Wordle.user.js
+// @updateURL    https://github.com/engelthehyp/Automation-for-World-Language-Games/raw/main/Game%20-%20Chinese%20Wordle.user.js
 // @version      1.0
 // @description  This script will automatically complete the game "Chinese Wordle" as soon as it is launched, giving the maximum of 4 points by default. You can change the number of points given to be any number, but under normal circumstances the maximum number of points is 4, so if you don't want to blow your cover, it may be better keep it that way.
 // @author       https://github.com/engelthehyp
