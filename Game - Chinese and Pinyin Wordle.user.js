9fcfcd072db00ce7d52107e6edc6f3bde31739eb
engelthehyp <engelthehyp@gmail.com>
2024-02-26 13:44:11 -0500

    Update Game - Chinese and Pinyin Wordle.user.js
    Description update, version number update, download/update URL update to reflect name change.

--- Game - Chinese and Pinyin Wordle.user.js
+++ Game - Chinese and Pinyin Wordle.user.js
@@ -2,16 +2,16 @@
 // @name         World Language Games - Automation for "Chinese Wordle"
 // @license      MIT
 // @namespace    https://github.com/engelthehyp/Automation-for-World-Language-Games
-// @version      1.0
-// @description  This script will automatically complete the game "Chinese Wordle" as soon as it is launched, giving the maximum of 4 points by default. You can change the number of points given to be any number, but under normal circumstances the maximum number of points is 4, so if you don't want to blow your cover, it may be better keep it that way.
+// @version      1.1
+// @description  This script will automatically complete "Chinese Wordle" or "Pinyin Wordle" (whichever game you started) as soon as it is launched, giving the maximum of 4 points by default. You can change the number of points given to be any number, but under normal circumstances the maximum number of points is 4, so if you don't want to blow your cover, it may be better keep it that way.
 // @author       https://github.com/engelthehyp
 // @match        *://wlangames.net/ChineseWordle.php*
 // @match        *://wlangames.net/PinyinWordle*.php*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=wlangames.net
 // @grant        none
 // @run-at       document-end
-// @downloadURL https://update.greasyfork.org/scripts/481125/World%20Language%20Games%20-%20Automation%20for%20%22Chinese%20Wordle%22.user.js
-// @updateURL https://update.greasyfork.org/scripts/481125/World%20Language%20Games%20-%20Automation%20for%20%22Chinese%20Wordle%22.meta.js
+// @downloadURL https://update.greasyfork.org/scripts/481125/World%20Language%20Games%20-%20Automation%20for%20%22Chinese%20and%20Pinyin%20Wordle%22.user.js
+// @updateURL https://update.greasyfork.org/scripts/481125/World%20Language%20Games%20-%20Automation%20for%20%22Chinese%20and%20Pinyin%20Wordle%22.meta.js
 // ==/UserScript==
 
 (function() {
